d7ec63d6ce4d277a75e07e4b65ecf93df25495d2
popobo <331361996@qq.com>
2022-11-07 21:11:02 +0800

    Use Element Buffer Object，EBO to draw a rectangle

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -80,18 +80,32 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+		0.5f, 0.5f, 0.0f,   // 右上角
+		0.5f, -0.5f, 0.0f,  // 右下角
+		-0.5f, -0.5f, 0.0f, // 左下角
+		-0.5f, 0.5f, 0.0f   // 左上角
 	};
+
+	unsigned int indices[] = {
+		// 注意索引从0开始! 
+		// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+		// 这样可以由下标代表顶点组合成矩形
+
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
+	};
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
 
 	unsigned int VAO;
 	glGenVertexArrays(1, &VAO);
-	//绑定VAO
-	glBindVertexArray(VAO);
 
 	unsigned int VBO;
 	glGenBuffers(1, &VBO);
+
+	//绑定VAO
+	glBindVertexArray(VAO);
 	//把新创建的缓冲绑定到GL_ARRAY_BUFFER目标上
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	//把之前定义的顶点数据复制到缓冲的内存中
@@ -100,6 +114,9 @@ int main()
 	//告诉OpenGL该如何解析顶点数据
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	while (!glfwWindowShouldClose(window)) {
 		//检测输入
@@ -113,7 +130,8 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		//检查并调用事件，交换缓冲
 		glfwPollEvents();
